fd55967e701bc2400946900621f836b5d580eada
FabianVarela <jfabian.303@gmail.com>
2025-07-23 09:25:56 -0600

    Initialize project

---
+++ android/app/src/main/kotlin/com/developer/fabian/shader/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.developer.fabian.shader_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:shader_app/app/app.dart';
+import 'package:shader_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
