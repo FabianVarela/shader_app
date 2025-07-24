e6f7efb25eef2deaed195cb5c1148fcf0bab4a67
FabianVarela <jfabian.303@gmail.com>
2025-07-24 12:17:05 -0600

    Add button to navigate to wave shader

--- lib/features/main/view/main_page.dart
+++ lib/features/main/view/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shader_app/features/wave/view/wave_page.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
@@ -15,8 +16,16 @@ class MainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Shaders example')),
-      body: Center(child: Container()),
+      appBar: AppBar(title: const Text('Shaders example')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute<void>(builder: (_) => const WavePage()),
+          ),
+          child: const Text('Wave shader'),
+        ),
+      ),
     );
   }
 }
