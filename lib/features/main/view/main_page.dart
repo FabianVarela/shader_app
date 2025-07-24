4b560f5c65333f40a6669169a5b7d15e9ef9d907
FabianVarela <jfabian.303@gmail.com>
2025-07-24 15:39:18 -0600

    Create pyramid shader page

--- lib/features/main/view/main_page.dart
+++ lib/features/main/view/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shader_app/features/pyramid/view/pyramid_page.dart';
 import 'package:shader_app/features/wave/view/wave_page.dart';
 
 class MainPage extends StatelessWidget {
@@ -15,17 +16,31 @@ class MainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final buttonList = <({String text, Widget page})>[
+      (text: 'Wave Shader', page: const WavePage()),
+      (text: 'Pyramid fractal Shader', page: const PyramidPage()),
+    ];
+
     return Scaffold(
       appBar: AppBar(title: const Text('Shaders example')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.push(
-            context,
-            MaterialPageRoute<void>(builder: (_) => const WavePage()),
-          ),
-          child: const Text('Wave shader'),
+      body: SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        child: Column(
+          spacing: 10,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            for (final item in buttonList)
+              ElevatedButton(
+                onPressed: () => _goToPage(context, item.page),
+                child: Text(item.text),
+              ),
+          ],
         ),
       ),
     );
+  }
+
+  void _goToPage(BuildContext context, Widget page) {
+    Navigator.push(context, MaterialPageRoute<void>(builder: (_) => page));
   }
 }
---
+++ lib/features/pyramid/view/pyramid_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_shaders/flutter_shaders.dart';
+
+class PyramidPage extends StatelessWidget {
+  const PyramidPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const PyramidView();
+  }
+}
+
+class PyramidView extends StatefulWidget {
+  const PyramidView({super.key});
+
+  @override
+  State<PyramidView> createState() => _PyramidViewState();
+}
+
+class _PyramidViewState extends State<PyramidView>
+    with SingleTickerProviderStateMixin {
+  late final Ticker _ticker;
+  double _time = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _ticker = createTicker((elapsed) {
+      _time += 0.015;
+      setState(() {});
+    });
+    _ticker.start();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.sizeOf(context);
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: ShaderBuilder(
+        (_, shader, child) => AnimatedSampler(
+          (_, size, canvas) {
+            final screenIncrement = switch (defaultTargetPlatform) {
+              TargetPlatform.android || TargetPlatform.iOS => 3,
+              _ => 1,
+            };
+
+            shader
+              ..setFloat(0, _time)
+              ..setFloat(1, size.width * screenIncrement)
+              ..setFloat(2, size.height * screenIncrement);
+            canvas.drawPaint(Paint()..shader = shader);
+          },
+          child: child!,
+        ),
+        assetKey: 'shaders/pyramid_fractal.frag',
+        child: SizedBox(width: screenSize.width, height: screenSize.height),
+      ),
+    );
+  }
+}
