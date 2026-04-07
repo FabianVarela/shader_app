b03c61b366a4301343845792176c62029472c2ff
FabianVarela <jfabian.303@gmail.com>
2026-04-06 20:27:44 -0500

    Generate assets file

---
+++ lib/app/gen/assets.gen.dart
@@ -0,0 +1,117 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+class $AssetsResourcesGen {
+  const $AssetsResourcesGen();
+
+  /// File path: assets/resources/dash.jpg
+  String get dash => 'assets/resources/dash.jpg';
+
+  /// File path: assets/resources/dash_vertical.jpg
+  String get dashVertical => 'assets/resources/dash_vertical.jpg';
+
+  /// File path: assets/resources/forest_slope_4k.jpg
+  String get forestSlope4k => 'assets/resources/forest_slope_4k.jpg';
+
+  /// File path: assets/resources/noise_rgba.png
+  String get noiseRgba => 'assets/resources/noise_rgba.png';
+
+  /// List of all assets
+  List<String> get values => [dash, dashVertical, forestSlope4k, noiseRgba];
+}
+
+class $AssetsThumbnailsGen {
+  const $AssetsThumbnailsGen();
+
+  /// File path: assets/thumbnails/art_coding.png
+  String get artCoding => 'assets/thumbnails/art_coding.png';
+
+  /// File path: assets/thumbnails/burn_effect.png
+  String get burnEffect => 'assets/thumbnails/burn_effect.png';
+
+  /// File path: assets/thumbnails/butterfly_flock.png
+  String get butterflyFlock => 'assets/thumbnails/butterfly_flock.png';
+
+  /// File path: assets/thumbnails/cronos_effect.png
+  String get cronosEffect => 'assets/thumbnails/cronos_effect.png';
+
+  /// File path: assets/thumbnails/cyberspace_warehouse.png
+  String get cyberspaceWarehouse =>
+      'assets/thumbnails/cyberspace_warehouse.png';
+
+  /// File path: assets/thumbnails/dive_cloud.png
+  String get diveCloud => 'assets/thumbnails/dive_cloud.png';
+
+  /// File path: assets/thumbnails/gradient_flow.png
+  String get gradientFlow => 'assets/thumbnails/gradient_flow.png';
+
+  /// File path: assets/thumbnails/hail_mary.png
+  String get hailMary => 'assets/thumbnails/hail_mary.png';
+
+  /// File path: assets/thumbnails/lava_lamp.png
+  String get lavaLamp => 'assets/thumbnails/lava_lamp.png';
+
+  /// File path: assets/thumbnails/mega_effect.png
+  String get megaEffect => 'assets/thumbnails/mega_effect.png';
+
+  /// File path: assets/thumbnails/plasma_effect.png
+  String get plasmaEffect => 'assets/thumbnails/plasma_effect.png';
+
+  /// File path: assets/thumbnails/pyramid_fractal.png
+  String get pyramidFractal => 'assets/thumbnails/pyramid_fractal.png';
+
+  /// File path: assets/thumbnails/ripple_effect.png
+  String get rippleEffect => 'assets/thumbnails/ripple_effect.png';
+
+  /// File path: assets/thumbnails/ripple_touch.png
+  String get rippleTouch => 'assets/thumbnails/ripple_touch.png';
+
+  /// File path: assets/thumbnails/seascape.png
+  String get seascape => 'assets/thumbnails/seascape.png';
+
+  /// File path: assets/thumbnails/sun_vortex.png
+  String get sunVortex => 'assets/thumbnails/sun_vortex.png';
+
+  /// File path: assets/thumbnails/warp_effect.png
+  String get warpEffect => 'assets/thumbnails/warp_effect.png';
+
+  /// File path: assets/thumbnails/wavy_stripes.png
+  String get wavyStripes => 'assets/thumbnails/wavy_stripes.png';
+
+  /// List of all assets
+  List<String> get values => [
+    artCoding,
+    burnEffect,
+    butterflyFlock,
+    cronosEffect,
+    cyberspaceWarehouse,
+    diveCloud,
+    gradientFlow,
+    hailMary,
+    lavaLamp,
+    megaEffect,
+    plasmaEffect,
+    pyramidFractal,
+    rippleEffect,
+    rippleTouch,
+    seascape,
+    sunVortex,
+    warpEffect,
+    wavyStripes,
+  ];
+}
+
+class Assets {
+  const Assets._();
+
+  static const $AssetsResourcesGen resources = $AssetsResourcesGen();
+  static const $AssetsThumbnailsGen thumbnails = $AssetsThumbnailsGen();
+}
